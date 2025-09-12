e095422c549c6df4f8b64a63781e926486c93cd0
Kobe <dev.skyachieve91@gmail.com>
2025-09-12 10:16:28 +0900

    :sparkles: Feat: first commit

---
+++ src/main/java/com/kobe/devkobehompage/DevkobeHompageApplication.java
@@ -0,0 +1,13 @@
+package com.kobe.devkobehompage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevkobeHompageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevkobeHompageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobe/devkobehompage/DevkobeHompageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobe.devkobehompage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevkobeHompageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
