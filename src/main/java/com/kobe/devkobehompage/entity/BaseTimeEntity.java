b17883c213d9b282bca3f272c76371ae58eab7fb
Kobe <dev.skyachieve91@gmail.com>
2025-09-14 06:08:33 +0900

    feat: 공통 속성인 생성/수정 시간을 관리하는 추상 클래스 생성

---
+++ src/main/java/com/kobe/devkobehompage/entity/BaseTimeEntity.java
@@ -0,0 +1,35 @@
+package com.kobe.devkobehompage.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * packageName    : com.kobe.devkobehompage.entity
+ * fileName       : BaseTimeEntity
+ * author         : kobe
+ * date           : 2025. 9. 14.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025. 9. 14.        kobe       최초 생성
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdDate;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+}
